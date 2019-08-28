f7d48bcad754838cb428eeed87c5e29e254f0d16
austi <austinjaynew@gmail.com>
2019-08-28 17:06:12 -0500

    Test

---
+++ src/main/java/Finder.java
@@ -0,0 +1,31 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Finder {
+
+
+
+
+
+public static int findMin(int[] intArray) {
+    Scanner input = new Scanner(System.in);
+    int array1[] = new int[] {-5, 0, 5, 10, 15};
+    Arrays.sort(array1);
+    return array1[0];
+
+}
+
+public static int findMax(int[] intArray) {
+    Scanner input = new Scanner(System.in);
+    int array1[] = new int[] {-5, 0, 5, 10, 15};
+    Arrays.sort(array1);
+    return array1[array1.length-1];
+}
+    public static void main(String[] args) {
+
+
+}
+
+
+}
--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -3,19 +3,22 @@ import org.junit.*;
 import static org.junit.Assert.assertEquals;
 
 public class JUnitTest {
-    int[] array1 = new int[] {-5, 0, 5, 10, 15};
-    @Test
-        public void test_findMax() {
 
-        int[] max = Finder.findMax(array1);
+    int[] arrayTest = new int[]{-5, 0, 5, 10, 15};
+
+    @Test
+    public void test_findMax() {
+
+        int max = Finder.findMax(arrayTest);
         Assert.assertEquals(max, 15);
 
     }
+
     @Test
     public void test_findMin() {
 
-        int[] min = Finder.findMin(array1);
-        Assert.assertEquals(min, 15);
+        int min = Finder.findMin(arrayTest);
+        Assert.assertEquals(min, -5);
 
     }
 
